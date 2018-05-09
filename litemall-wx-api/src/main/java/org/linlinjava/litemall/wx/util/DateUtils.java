2a7e4d5bfd7f106d603cab1c3a66f0c05490282d
lanye123 <627442511@qq.com>
2018-05-09 11:52:54 +0800

    获取周榜接口

---
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/util/DateUtils.java
@@ -0,0 +1,146 @@
+package org.linlinjava.litemall.wx.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * @Description TODO
+ * @Author lanye
+ * @Date 2018/5/9 10:58
+ **/
+public class DateUtils {
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static void main(String args[]){
+        System.out.println(getCurrentMondayDate());
+        System.out.println(getMinMonthDate("2017-04-17"));
+        System.out.println(getMaxMonthDate("2017-04-17"));
+    }
+    // 获得本周一与当前日期相差的天数
+    /**
+      * @author lanye
+      * @Description 获得本周一与当前日期相差的天数
+      * @Date 2018/5/9 11:17
+      * @Param []
+      * @return int
+      **/
+    public static  int getMondayPlus() {
+        Calendar cd = Calendar.getInstance();
+        int dayOfWeek = cd.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek == 1) {
+            return -6;
+        } else {
+            return 2 - dayOfWeek;
+        }
+    }
+
+    /**
+      * @author lanye
+      * @Description TODO
+      * @Date 2018/5/9 11:30
+      * @Param [simpleDateFormat]
+      * @return java.lang.String
+      **/
+    public static  String getCurrentMonday(SimpleDateFormat simpleDateFormat) {
+        Date monday = getCurrentMondayDate();
+        DateFormat df = DateFormat.getDateInstance();
+        if(simpleDateFormat != null){
+            return simpleDateFormat.format(monday);
+        }
+        return df.format(monday);
+    }
+
+    /**
+      * @author lanye
+      * @Description 获得当前周周一的开始时间
+      * @Date 2018/5/9 11:27
+      * @Param []
+      * @return java.util.Date
+      **/
+    public static Date getCurrentMondayDate() {
+        int mondayPlus = getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus);
+        currentDate.set(Calendar.HOUR_OF_DAY,0);
+        currentDate.set(Calendar.MINUTE,0);
+        currentDate.set(Calendar.SECOND,0);
+        Date monday = currentDate.getTime();
+        return monday;
+    }
+
+    /**
+      * @author lanye
+      * @Description 获得当前周- 周日的日期
+      * @Date 2018/5/9 11:17
+      * @Param []
+      * @return java.lang.String
+      **/
+    public static String getPreviousSunday(SimpleDateFormat simpleDateFormat) {
+        Date monday = getPreviousSundayDate();
+        DateFormat df = DateFormat.getDateInstance();
+        if(simpleDateFormat != null){
+            return simpleDateFormat.format(monday);
+        }
+        return df.format(monday);
+    }
+
+    /**
+      * @author lanye
+      * @Description 获得当前周周日的截止时间
+      * @Date 2018/5/9 11:31
+      * @Param []
+      * @return java.util.Date
+      **/
+    public static Date getPreviousSundayDate() {
+        int mondayPlus = getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus +6);
+        currentDate.set(Calendar.HOUR_OF_DAY,0);
+        currentDate.set(Calendar.MINUTE,0);
+        currentDate.set(Calendar.SECOND,0);
+        Date monday = currentDate.getTime();
+        return monday;
+    }
+
+    /**
+      * @author lanye
+      * @Description 获得当前月--开始日期
+      * @Date 2018/5/9 11:17
+      * @Param [date]
+      * @return java.lang.String
+      **/
+    public static String getMinMonthDate(String date) {
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(dateFormat.parse(date));
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+            return dateFormat.format(calendar.getTime());
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+      * @author lanye
+      * @Description 获得当前月--结束日期
+      * @Date 2018/5/9 11:18
+      * @Param [date]
+      * @return java.lang.String
+      **/
+    public static String getMaxMonthDate(String date){
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(dateFormat.parse(date));
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+            return dateFormat.format(calendar.getTime());
+        }  catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/MedalDetailsController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/MedalDetailsController.java
@@ -6,11 +6,15 @@ import org.linlinjava.litemall.db.service.LitemallUserService;
 import org.linlinjava.litemall.db.service.MedalDetailsService;
 import org.linlinjava.litemall.db.util.ResponseUtil;
 import org.linlinjava.litemall.wx.annotation.LoginUser;
+import org.linlinjava.litemall.wx.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 @RestController
@@ -41,14 +45,13 @@ public class MedalDetailsController {
             medalDetails.setAmount(medalDetailsService.getScoreByUserId(medalDetails.getUserId(),null,null));
         }
         //对用户成长值进行排序
-        Collections.sort(medalDetailsList, (s1, s2) ->{// 省略参数表的类型
-            if(s1 == null)
-                return -1;
-            if(s2 == null)
-                return 1;
-            return s2.getAmount()-s1.getAmount();
-        });
+        sort(medalDetailsList);
+        Integer userIdDb = null;
         for(MedalDetails medalDetails:medalDetailsList){
+            if(userIdDb == medalDetails.getUserId()){
+                continue;
+            }
+            userIdDb = medalDetails.getUserId();
             dataItem = new HashMap<>();
             medal = medalDetailsService.getMedalByScore(medalDetailsService.getScoreByUserId(medalDetails.getUserId(),null,null));
 
@@ -65,6 +68,79 @@ public class MedalDetailsController {
 
         data.put("returnTotalList",returnTotalList);
         return ResponseUtil.ok(data);
+    }
+
+    /**
+     *@Author:lanye
+     *@Description:获取周榜接口
+     *@Date:10:30 2018/5/9
+     */
+    @GetMapping("weekList")
+    public Object getWeekList(@LoginUser Integer userId){
+        if(userId == null){
+            return ResponseUtil.unlogin();
+        }
+        //拼装时间参数
+        Date date = DateUtils.getCurrentMondayDate();
+        Instant instant = date.toInstant();
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime time1 = instant.atZone(zoneId).toLocalDateTime();
+        date = DateUtils.getPreviousSundayDate();
+        instant = date.toInstant();
+        zoneId = ZoneId.systemDefault();
+        LocalDateTime time2 = instant.atZone(zoneId).toLocalDateTime();
+
+        Map<String,Object> data = new HashMap<>();
+        Map<String,Object> dataItem;
+        List<Map<String,Object>> returnTotalList = new ArrayList<>();
+        Medal medal;
+        List<MedalDetails> medalDetailsList = medalDetailsService.selectList(null,null,null,null,null,null);
+        for(MedalDetails medalDetails:medalDetailsList){
+            medalDetails.setAmount(medalDetailsService.getScoreByUserId(medalDetails.getUserId(),time1,time2));
+        }
+        //对用户成长值进行排序
+        sort(medalDetailsList);
+        Integer userIdDb = null;
+        for(MedalDetails medalDetails:medalDetailsList){
+            //剔除重复用户
+            if(userIdDb == medalDetails.getUserId()){
+                continue;
+            }
+            userIdDb = medalDetails.getUserId();
+            dataItem = new HashMap<>();
+            medal = medalDetailsService.getMedalByScore(medalDetailsService.getScoreByUserId(medalDetails.getUserId(),time1,time2));
+
+            dataItem.put("score",medalDetails.getAmount());
+            dataItem.put("userName",litemallUserService.findById(medalDetails.getUserId()).getUsername());
+            dataItem.put("medalName",medal.getName());
+            dataItem.put("imgUrl",medal.getImgUrl());
+            dataItem.put("comment",medal.getComment());
+            dataItem.put("max",medal.getMax());
+            dataItem.put("min",medal.getMin());
+
+            returnTotalList.add(dataItem);
+        }
+
+        data.put("returnTotalList",returnTotalList);
+        return ResponseUtil.ok(data);
+    }
+
+    /**
+      * @author lanye
+      * @Description 对用户成长值进行排序
+      * @Date 2018/5/9 11:42
+      * @Param [medalDetailsList]
+      * @return java.util.List<org.linlinjava.litemall.db.domain.MedalDetails>
+      **/
+    private List<MedalDetails> sort(List<MedalDetails> medalDetailsList){
+        Collections.sort(medalDetailsList, (s1, s2) ->{
+            if(s1 == null)
+                return -1;
+            if(s2 == null)
+                return 1;
+            return s2.getAmount()-s1.getAmount();
+        });
+        return medalDetailsList;
     }
 
 }
