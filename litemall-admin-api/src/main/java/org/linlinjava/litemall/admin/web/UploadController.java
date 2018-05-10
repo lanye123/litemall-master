89caf7f03b31c49b4c1b16dfc72ced0ce3a4eeac
774961477 <774961477@qq.com>
2018-05-11 01:35:15 +0800

    图文首页图片上传

---
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/UploadController.java
@@ -0,0 +1,152 @@
+package org.linlinjava.litemall.admin.web;
+
+import org.apache.commons.lang3.StringUtils;
+import org.linlinjava.litemall.db.domain.Article;
+import org.linlinjava.litemall.db.service.ArticleService;
+import org.linlinjava.litemall.db.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 文件上传接口开发
+    *@Author:LeiQiang
+    *@Description:
+    *@Date:0:01 2018/5/11
+    */
+@RestController
+@RequestMapping("/admin/upload")
+public class UploadController {
+    @Autowired
+    private ArticleService articleService;
+    public static final String ROOT = "upload-dir";
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public UploadController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+    /**
+     * 文件上传根目录(在Spring的application.yml的配置文件中配置):<br>
+     * web:
+     * upload-path: （jar包所在目录）/resources/static/
+     */
+    @Value("${web.upload-path}")
+    private String webUploadPath;
+     /** 文章管理模块图片上传
+     * @param file 图片
+     * @param articleId 文章ID
+     * @return
+     */
+    @PostMapping(value = "/imageUpload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object imageUpload(@RequestParam("file") MultipartFile file, Integer articleId) {
+        Map<String, Object> map = new HashMap<>();
+        if (!file.isEmpty()) {
+            if (file.getContentType().contains("image")) {
+                try {
+                    String temp = "images" + File.separator + "upload" + File.separator;
+                    // 获取图片的文件名
+                    String fileName = file.getOriginalFilename();
+                    // 获取图片的扩展名
+                    String extensionName = StringUtils.substringAfter(fileName, ".");
+                    // 新的图片文件名 = 获取时间戳+"."图片扩展名
+                    String newFileName = String.valueOf(System.currentTimeMillis()) + "." + extensionName;
+                    // 数据库保存的目录
+                    String datdDirectory = temp.concat(String.valueOf(articleId)).concat(File.separator);
+                    // 文件路径
+                    String filePath = webUploadPath.concat(datdDirectory);
+
+                    File dest = new File(filePath, newFileName);
+                    if (!dest.getParentFile().exists()) {
+                        dest.getParentFile().mkdirs();
+                    }
+                    // 判断文章是否上传的有图片，如果有则物理删除老照片
+                    Article article = articleService.findById(articleId);
+                    if (StringUtils.isNotBlank(article.getPhotoUrl())) {
+                        String oldFilePath = webUploadPath.concat(article.getPhotoUrl());
+                        File oldFile = new File(oldFilePath);
+                        if (oldFile.exists()) {
+                            oldFile.delete();
+                        }
+                    }
+                    // 上传到指定目录
+                    file.transferTo(dest);
+                    // 将反斜杠转换为正斜杠
+                    String data = datdDirectory.replaceAll("\\\\", "/") + newFileName;
+                    map.put("tempPath",temp);
+                    map.put("fileName",fileName);
+                    map.put("extensionName",extensionName);
+                    map.put("newFileName",newFileName);
+                    map.put("filePath",filePath);
+                    map.put("data",data);
+                    ResponseUtil.ok(map);
+                } catch (IOException e) {
+                    ResponseUtil.fail(0,"上传失败");
+                }
+            } else {
+                ResponseUtil.fail(0,"上传的文件不是图片类型，请重新上传!");
+            }
+        } else {
+            return ResponseUtil.fail(0,"上传失败，请选择要上传的图片!");
+        }
+        return ResponseUtil.ok(map);
+    }
+
+    @PostMapping(value = "/fileUpload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object fileupload(@RequestParam("file") MultipartFile file) {
+        Map<String, Object> map = new HashMap<>();
+        if (!file.isEmpty()) {
+            try {
+                String temp = "images" + File.separator + "upload" + File.separator;
+                // 获取图片的文件名
+                String fileName = file.getOriginalFilename();
+                // 获取图片的扩展名
+                String extensionName = StringUtils.substringAfter(fileName, ".");
+                // 新的图片文件名 = 获取时间戳+"."图片扩展名
+                String newFileName = String.valueOf(System.currentTimeMillis()) + "." + extensionName;
+                // 文件路径
+                String filePath = webUploadPath.concat(temp);
+
+                File dest = new File(filePath, newFileName);
+                if (!dest.getParentFile().exists()) {
+                    dest.getParentFile().mkdirs();
+                }
+                // 上传到指定目录
+                file.transferTo(dest);
+                // 将反斜杠转换为正斜杠
+                String data = temp.replaceAll("\\\\", "/") + newFileName;
+                map.put("tempPath",temp);
+                map.put("fileName",fileName);
+                map.put("extensionName",extensionName);
+                map.put("newFileName",newFileName);
+                map.put("filePath",filePath);
+                map.put("data",data);
+                ResponseUtil.ok(map);
+            } catch (IOException e) {
+                ResponseUtil.fail(0, "上传失败");
+            }
+        }
+        return ResponseUtil.ok(map);
+    }
+
+
+    @GetMapping(value = "/filelname")
+    public Object getFile(String filename) {
+        try {
+            return ResponseUtil.ok(resourceLoader.getResource("file:" + Paths.get(webUploadPath, filename).toString()));
+        } catch (Exception e) {
+            return ResponseUtil.fail();
+        }
+    }
+}
+
---
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/WebAppConfig.java
@@ -0,0 +1,40 @@
+package org.linlinjava.litemall.admin.web;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @ClassName: WebAppConfig
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Administrator
+ * @date 2017年7月11日
+ */
+/*@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+    private final Log logger = LogFactory.getLog(WebAppConfig.class);
+    //获取配置文件中图片的路径
+    @Value("${cbs.imagesPath}")
+    private String mImagesPath;
+
+    //访问图片方法
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (mImagesPath.equals("") || mImagesPath.equals("${cbs.imagesPath}")) {
+            String imagesPath = WebAppConfig.class.getClassLoader().getResource("").getPath();
+            if (imagesPath.indexOf(".jar") > 0) {
+                imagesPath = imagesPath.substring(0, imagesPath.indexOf(".jar"));
+            } else if (imagesPath.indexOf("classes") > 0) {
+                imagesPath = "file:" + imagesPath.substring(0, imagesPath.indexOf("classes"));
+            }
+            imagesPath = imagesPath.substring(0, imagesPath.lastIndexOf("/")) + "/images/";
+            mImagesPath = imagesPath;
+        }
+        registry.addResourceHandler("/images/**").addResourceLocations(mImagesPath);
+        super.addResourceHandlers(registry);
+    }
+}*/
