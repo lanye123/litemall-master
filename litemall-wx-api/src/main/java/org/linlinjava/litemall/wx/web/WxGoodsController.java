a9f3dadc1f9f41c08e4466324cd11ea571351c1f
leiqiang <774961477@qq.com>
2018-06-22 17:24:42 +0800

    1.订购单确认 2.订单确认（积分支付）

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
@@ -461,7 +461,7 @@ public class WxGoodsController {
         }
 
         List<LitemallAd> adList = litemallAdService.querySelective("","",null,null,"","");
-        info.setFlag(0);//显示邀请好友按钮1显示参团按钮，如果已经参团
+        info.setIsexist(0);//显示邀请好友按钮1显示参团按钮，如果已经参团
         data.put("adList", adList);
         data.put("userVoList", userVoList);
         data.put("collageDetailList", collageDetailList);
@@ -528,10 +528,10 @@ public class WxGoodsController {
         //查询参团人是否针对该商品已经参团
         CollageDetail collageDetail = collageDetailService.queryByPid(orderId,userId);
         if(collageDetail!=null) {
-            info.setFlag(1);
+            info.setIsexist(1);
         }
         else {
-            info.setFlag(0);
+            info.setIsexist(0);
         }
         data.put("adList", adList);
         data.put("userVoList", userVoList);
