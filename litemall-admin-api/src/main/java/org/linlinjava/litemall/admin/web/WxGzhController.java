fc2702220296fce5553a3c9b8c8fe2d6f00b85f9
leiqiang <774961477@qq.com>
2018-08-02 11:37:43 +0800

    公众号管理

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/WxGzhController.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/WxGzhController.java
@@ -1,11 +1,18 @@
 package org.linlinjava.litemall.admin.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * ClassName WxGzhController
- * Description TODO
+ * Description TODO 公众号管理模块功能开发（模板列表管理、粉丝管理、模板消息管理）
  * Author leiqiang
  * Date 2018/8/2 11:33
  * Version 1.0
  **/
+@RestController
+@RequestMapping("/admin/gzh")
 public class WxGzhController {
+
 }
